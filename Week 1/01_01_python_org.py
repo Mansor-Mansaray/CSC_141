6c05ee003dc1a1fe9ca2dd703f314424b4c90735
MansorMansaray <mansor.amond001@albright.edu>
2024-08-30 14:58:38 -0400

    Week 1

---
+++ Week 1/01_01_python_org.py
@@ -0,0 +1,14 @@
+...
+
+I like the success stories that are shown
+specifically the one for the art section
+Building Robust Codebases with Python's 
+Type Annotations by John Lekberg caught 
+my eye as he talks about the millions of line 
+of code that has been collected over the years
+by hundreds of different developers. learning 
+about things like this makes me excited for the 
+multiple challenges that will come up and will 
+takedown in the future.
+
+...
