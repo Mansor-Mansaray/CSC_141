20820bff5b9ec695af7ddbad5dfba601b8a390b1
MansorMansaray <mansor.amond001@albright.edu>
2024-09-03 09:46:52 -0400

    First Commit

---
+++ Week 1/Week 01/01_02 hello_world.py
@@ -0,0 +1,7 @@
+# There is a error within this line of code
+
+print (hello "world")
+
+# The problem is that the the quotation marks are only on world
+
+print ("Hello World")
---
+++ Week 1/Week 01/01_03 Infinite_skills.py
@@ -0,0 +1,12 @@
+...
+
+
+# If I had infinite skill in programming I would most likely create the first-ever real-time
+# AI for video games NPC and events they will react to everything the player does and what is
+# happening in the environment Like SAO(Sword Art Online) but without the whole kill switch 
+# in the headset. I would also like to make The Baseeline test from Blade Runner 2049, It would
+# be a great thing to check on police officer mental State and to check if they are fit to be 
+# on the feild or even be in the office.
+
+
+...
