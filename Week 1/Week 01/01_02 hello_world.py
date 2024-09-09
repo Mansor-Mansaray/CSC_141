d7287b8d7b87beff29574655da81681b3f8f15e1
Mansor Amond-Mansaray <mansor.amond001@albright.edu>
2024-09-09 13:43:52 -0400

    Update and rename 01_02 hello_world.py to 01_02_hello_world.py

--- Week 1/Week 01/01_02 hello_world.py
+++
@@ -1,7 +0,0 @@
-# There is a error within this line of code
-
-print (hello "world")
-
-# The problem is that the the quotation marks are only on world
-
-print ("Hello World")
---
+++ Week 1/Week 01/01_02_hello_world.py
@@ -0,0 +1,7 @@
+# There is an error within this line of code
+
+print (hello "world")
+
+# The problem is that the quotation marks are only on world
+
+print ("Hello World")
