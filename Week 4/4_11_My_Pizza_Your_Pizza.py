58e710da0d2396d070dd4224ed3837e679fa9953
Mansor Amond-Mansaray <mansor.amond001@albright.edu>
2024-11-07 10:14:07 -0500

    Update 4_11_My_Pizza_Your_Pizza.py

--- Week 4/4_11_My_Pizza_Your_Pizza.py
+++ Week 4/4_11_My_Pizza_Your_Pizza.py
@@ -1,14 +1,14 @@
-pizzas = ['Supreme', 'BBQ Chicken', 'Four Cheese', 'Meat Lovers']
+pizzas = ['supreme', 'bbq chicken', 'four Cheese', 'meat lovers']
 for pizza in pizzas:
     print(f"{pizza.title()}, Is a top 4 pizza.")
 
 print (f"{pizza.title()} is extremly good pizza.\nA nice and pipping hot pizza is always the best.\nIf you touch my pizza I will fight you.")
 
 
-Message = "My friend’s favorite pizzas are:"
-print(Message)
+message = "my friend’s favorite pizzas are:"
+print(message)
 
-friend_pizzas = ['Pepperoni', 'Buffalo Chicken', 'Cheese', 'Margarita']
+friend_pizzas = ['pepperoni', 'buffalo chicken', 'cheese', 'margarita']
 for Pizza in friend_pizzas:
     print(f"{Pizza.title()}, Is a top 4 pizza.")
 
