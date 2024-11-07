002968c02aab06e7af58671242dce02eaa4d840d
Mansor Amond-Mansaray <mansor.amond001@albright.edu>
2024-11-07 10:16:12 -0500

    Update 4_14_PEP_8.py

--- Week 4/4_14_PEP_8.py
+++ Week 4/4_14_PEP_8.py
@@ -1,9 +1,10 @@
 '''
-While Reading throught the python pep 8 I learned that
-there controverse with names the start with _.I also 
-Learned that Line breaksin binary code cancause operators to get
+While Reading through the Python Pep 8 I learned that
+there is controversy with names that start with _. I also 
+Learned that Line breaks in binary code can cause operators to get
 scattered in between all of the columns. The final thing
-that I learned that double and single string quote are
-exactly the same.
+that I learned is that double and single-string quotes are
+the same.
+
 
 '''
